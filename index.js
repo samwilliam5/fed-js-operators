76939fdd4f259687969c9d32cea9f31efe041f8c
william5 <samwilliamj333@gmail.com>
2022-09-10 11:21:15 +0530

    arithmetic operators

---
+++ index.js
@@ -0,0 +1,61 @@
+//Arithmetic operators
+let x=10;
+let y=20;
+console.log(`Aruthmetic operators: ${x+y} `);
+// console.log(x-y);
+// console.log(x*y);
+// console.log(x/y);
+// console.log(x%y);
+// ++	Increment
+// --	Decrement
+
+
+// Assignment operators
+let f = 10;
+f += 5;
+// f-=5;
+// f*=5;
+// f/=5;
+// f%=5;
+
+console.log(`Assignment operators: ${f}`);
+
+
+//+ assign operator for string
+
+let text = "Hello ";
+text += "All";
+
+console.log(`assign operators for  concat string: ${text}`);
+
+
+//comparison operators
+var person1='5';
+
+
+console.log(`Double == ${person1==5}`);
+console.log(`Triple === ${person1===5}`);
+
+var l=4;
+var t=20;
+
+console.log(`!=${l!=t}`);
+// console.log(l<t);
+// console.log(l>t);
+// console.log(l>=t);
+// console.log(l<=t);
+
+
+//ternary operator
+var v=20;
+console.log(`Ternary Operators : ${v>40 ? false : true } `);
+
+
+//Logical operators
+var h = 5;
+console.log(`&&: ${h > 3 && h < 10}`);
+
+
+var u = 5;
+console.log(u > 3 || u < 2);
+
